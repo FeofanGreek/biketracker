b853dbeb1be253ed72f2f9711095d69a666d7bc4
Дмитрий Колдашев <dmitry@koldashev.ru>
2026-09-03 08:49:59 +0300

    Смещение карты делаем только кода идет запись трека

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -8,7 +8,7 @@ import 'package:latlong2/latlong.dart';
 import 'package:vector_map_tiles/vector_map_tiles.dart';
 
 import '../main.dart';
-import '../secrets.dart';
+//import '../secrets.dart';
 import '../widgets/chart_height.dart';
 
 Future<Style> _readStyle() => StyleReader(
